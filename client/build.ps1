3a74f8c0553e1fd7766c1c7317c95aff75294e80
Arjun Mehta <aakash.mehta18@gmail.com>
2024-02-08 23:23:20 -0500

    made client more flexible in urls and base imaage;

--- client/build.ps1
+++ client/build.ps1
@@ -1,5 +1,8 @@
 $IMAGE="rockylinux:8-minimal"
 $NODE="https://nodejs.org/dist/v20.10.0/node-v20.10.0-linux-x64.tar.xz"
 $MINICONDA="https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+$YUM_REPO="http://localhost:8080/repos/config/rocky"
+$NPM_REPO="http://localhost:8080/repos/config/npm"
+$PIP_REPO="http://localhost:8080/repos/config/pip"
 
-docker build --network=host -t unirepo-client . --build-arg IMAGE=$IMAGE --build-arg NODE=$NODE --build-arg MINICONDA=$MINICONDA
+docker build --network=host -t unirepo-client . --build-arg IMAGE=$IMAGE --build-arg NODE=$NODE --build-arg MINICONDA=$MINICONDA --build-arg YUM_REPO=$YUM_REPO --build-arg NPM_REPO=$NPM_REPO --build-arg PIP_REPO=$PIP_REPO
